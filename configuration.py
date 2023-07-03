dbff041f0e303706da68a9b4f7d2e082f5494da9
Pavel Popov <popov.pavel.94@mail.ru>
2023-07-03 19:54:58 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,9 @@
+URL_SERVICE = "https://48d47dc4-bcd9-4f78-8305-34ff629c4d7a.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
+
+
+
